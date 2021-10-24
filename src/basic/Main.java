83c7ba8d21580da5d3b311651d5bf03471026332
Bodakros <90177876+Bodakros@users.noreply.github.com>
2021-10-24 18:37:27 +0300

    second

--- src/basic/Main.java
+++ src/basic/Main.java
@@ -2,6 +2,11 @@ package basic;
 
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hello");
+     Zoo a = new Zoo("Zoo_3000" , "Ivanucivka", 100);
+        System.out.println(a.name);
+        System.out.println(a.location);
+        System.out.println(a.price);
+
+        System.out.println( );
     }
 }
--- src/basic/Zoo.java
+++ src/basic/Zoo.java
@@ -1,5 +1,23 @@
 package basic;
 
 public class Zoo{
+    String name;
+    String location;
+    int price;
 
+ public Zoo(String name, String location, int price){
+     this.name = name;
+     this.location = location;
+     this.price = price;
+ }
+    public int pay(int cash) {
+        if (cash >= price) {
+            int result = cash - price;
+            return result;
+        }
+         else  {
+            int result = price - cash;
+            throw new ArithmeticException("Замало коштів, будь ласка додайте " + result);
+         }
+ }
 }
