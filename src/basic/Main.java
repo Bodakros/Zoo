b5399e53ea847ec863ddfa92b5b4533100891027
Bodakros <90177876+Bodakros@users.noreply.github.com>
2021-10-30 15:50:56 +0300

    start2

--- src/basic/Main.java
+++ src/basic/Main.java
@@ -5,8 +5,9 @@ public class Main {
      Zoo a = new Zoo("Zoo_3000" , "Ivanucivka", 100);
         System.out.println(a.name);
         System.out.println(a.location);
-        System.out.println(a.price);
 
-        System.out.println( );
+        System.out.println(a.pay (200) );
+        System.out.println(a.pay (100) );
+        System.out.println(a.pay (50) );
     }
 }
